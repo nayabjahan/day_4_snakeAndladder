70ce780249a2017ed1e6e53ab9f4f354d9f4b550
nayab jahan <nayabzafir@gmail.com>
2022-06-28 19:57:10 +0530

    Added:Check for otion

---
+++ CheckOption.java
@@ -0,0 +1,32 @@
+public class CheckOption {
+    public static final int noplay= 0;
+    public static final int ladder = 1;
+    public static final int snake = 2;
+
+    public static void main(String[] args){
+        System.out.println("welcome to snake and ladder game");
+        int position = 0;
+        System.out.println("player position:"+position);
+
+        int roll_die = (int)(Math.floor(Math.random() * 10) % 6 ) + 1;
+        int check_option=(int)(Math.floor(Math.random() * 10) % 3);
+        switch (check_option) {
+            case noplay:
+                position = 0;
+                System.out.println("its a noplay! stays here in the same place");
+                break;
+            case ladder:
+                position += roll_die;
+                System.out.println("its a ladder! player moves ahead by" + roll_die);
+                break;
+            case snake:
+                position -= roll_die;
+                System.out.println("ita a snake! player moves behind by" + roll_die);
+                break;
+            default:
+                break;
+        }
+        }
+
+
+}
--- RollDies.java
+++ RollDies.java
@@ -1,7 +1,7 @@
 public class RollDies {
     public static void main(String[] args){
         System.out.println("welcome to snake and ladder game");
-
+//getting random number on rolling dies from 1 to6
         System.out.println(((Math.random() * 10 % 6) + 1));
 }
 }
