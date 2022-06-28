81024ab4da563dde4e7dd328835fda6999bf1505
nayab jahan <nayabzafir@gmail.com>
2022-06-28 13:34:52 +0530

    Added:Snake And Ladder

---
+++ SnakeAndLadder.java
@@ -0,0 +1,9 @@
+public class SnakeAndLadder {
+    public static void main(String[] args){
+        System.out.println("welcome to snake and ladder game");
+        int position = 0;
+        System.out.println("player position:"+position);
+        int player1 = (int)((Math.random()*10)%1);
+        System.out.println(player1);
+    }
+}
